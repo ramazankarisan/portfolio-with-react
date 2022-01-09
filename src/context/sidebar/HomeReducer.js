707a3c868fd3dbbc57483468c1a10ac39f10dd57
ramazankarisan <rkarisan25@gmail.com>
2022-01-09 15:27:30 +0100

    portfolio dimmer action added

--- src/context/sidebar/HomeReducer.js
+++ src/context/sidebar/HomeReducer.js
@@ -1,7 +1,11 @@
 export const sidebarReducer = (state, action) => {
   switch (action.type) {
     case 'MENU_OPEN':
-      return { menuShow: !state.menuShow }
+      return { menuShow: !state.menuShow };
+    case 'HANDLE_SHOW':
+      return { ...state, active: true };
+    case 'HANDLE_HIDE':
+      return { ...state, active: false }
     default:
       return state;
   }
--- src/context/sidebar/HomeState.js
+++ src/context/sidebar/HomeState.js
@@ -3,14 +3,20 @@ import homeContext from './homeContext'
 import { sidebarReducer } from './HomeReducer'
 
 const HomeState = (props) => {
-  const [state, dispatch] = useReducer(sidebarReducer, { menuShow: true });
+  const [state, dispatch] = useReducer(sidebarReducer, { menuShow: true, active: false });
 
   const handleClick = () => {
     dispatch({ type: 'MENU_OPEN' });
   }
 
+  const handleShow = () => {
+    dispatch({ type: 'HANDLE_SHOW' })
+  }
+  const handleHide = () => {
+    dispatch({ type: 'HANDLE_HIDE' })
+  }
   return (
-    <homeContext.Provider value={{ state, dispatch, handleClick }}>
+    <homeContext.Provider value={{ state, handleClick, handleShow, handleHide }}>
       {props.children}
     </homeContext.Provider>
   )
